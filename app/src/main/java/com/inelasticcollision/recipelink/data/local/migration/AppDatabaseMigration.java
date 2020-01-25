912651d766b62e72d3c433ee48a0892f5cbef69a
Tyler Madonna <developer@madonnaapps.com>
2020-01-25 05:45:10 -0500

    Changed AppDatabaseMigration to insert null into database if image url, notes, and tags are empty

--- app/src/main/java/com/inelasticcollision/recipelink/data/local/migration/AppDatabaseMigration.java
+++ app/src/main/java/com/inelasticcollision/recipelink/data/local/migration/AppDatabaseMigration.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 
 import androidx.annotation.NonNull;
+import androidx.room.OnConflictStrategy;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
@@ -32,23 +33,31 @@ public final class AppDatabaseMigration {
                     "PRIMARY KEY(id));");
 
             Cursor cursor = db.query("SELECT date_updated, title, link, photo_url, favorite, notes, keywords FROM recipe;");
-            db.beginTransaction();
-            try {
-                while (cursor.moveToNext()) {
-                    ContentValues values = new ContentValues(8);
-                    values.put("id", UUID.randomUUID().toString());
-                    values.put("last_modified", cursor.getLong(0));
-                    values.put("title", cursor.getString(1));
-                    values.put("url", cursor.getString(2));
-                    values.put("image_url", cursor.getString(3));
-                    values.put("favorite", cursor.getInt(4));
-                    values.put("notes", cursor.getString(5));
-                    values.put("tags", cursor.getString(6));
+            if (cursor.getCount() > 0) {
+                db.beginTransaction();
+                try {
+                    while (cursor.moveToNext()) {
+                        String imageUrl = cursor.getString(3);
+                        String notes = cursor.getString(5);
+                        String tags = cursor.getString(6);
+
+                        ContentValues values = new ContentValues(8);
+                        values.put("id", UUID.randomUUID().toString());
+                        values.put("last_modified", cursor.getLong(0));
+                        values.put("title", cursor.getString(1));
+                        values.put("url", cursor.getString(2));
+                        values.put("image_url", (imageUrl == null || imageUrl.isEmpty()) ? null : imageUrl);
+                        values.put("favorite", cursor.getInt(4));
+                        values.put("notes", (notes == null || notes.isEmpty()) ? null : notes);
+                        values.put("tags", (tags == null || tags.isEmpty()) ? null : tags);
+
+                        db.insert("recipes", OnConflictStrategy.REPLACE, values);
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                    cursor.close();
                 }
-                db.setTransactionSuccessful();
-            } finally {
-                db.endTransaction();
-                cursor.close();
             }
 
             db.execSQL("DROP TABLE IF EXISTS recipe;");
