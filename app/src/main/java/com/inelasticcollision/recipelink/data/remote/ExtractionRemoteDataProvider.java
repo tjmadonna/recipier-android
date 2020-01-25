c460b2ccf81b3394c57d94bf2eaac33addc6b854
Tyler Madonna <developer@madonnaapps.com>
2020-01-24 21:53:22 -0500

    Changed ExtractionRemoteDataProvider to be a singleton

--- app/src/main/java/com/inelasticcollision/recipelink/data/remote/ExtractionRemoteDataProvider.java
+++ app/src/main/java/com/inelasticcollision/recipelink/data/remote/ExtractionRemoteDataProvider.java
@@ -8,22 +8,87 @@
 
 package com.inelasticcollision.recipelink.data.remote;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.inelasticcollision.recipelink.BuildConfig;
 import com.inelasticcollision.recipelink.data.remote.models.Response;
 import com.inelasticcollision.recipelink.data.remote.models.Result;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.functions.Function;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ExtractionRemoteDataProvider implements RemoteDataProvider {
 
     private final ExtractionService mExtractionService;
 
-    public ExtractionRemoteDataProvider(ExtractionService extractionService) {
+    @NonNull
+    private static final Object lock = new Object();
+    @Nullable
+    private static volatile ExtractionRemoteDataProvider instance = null;
+
+    private ExtractionRemoteDataProvider(ExtractionService extractionService) {
         mExtractionService = extractionService;
     }
 
+    @NonNull
+    public static ExtractionRemoteDataProvider getInstance(@NonNull Context context) {
+        if (instance == null) {
+            synchronized (lock) {
+                if (instance == null) {
+
+                    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .readTimeout(30, TimeUnit.SECONDS)
+                            .connectTimeout(30, TimeUnit.SECONDS)
+                            .addInterceptor(new Interceptor() {
+                                @Override
+                                public okhttp3.Response intercept(Chain chain) throws IOException {
+                                    // Inject api key into query parameters
+                                    HttpUrl url = chain.request()
+                                            .url()
+                                            .newBuilder()
+                                            .addQueryParameter("key", BuildConfig.REMOTE_SERVICE_API_KEY)
+                                            .build();
+                                    Request request = chain.request()
+                                            .newBuilder()
+                                            .url(url)
+                                            .build();
+                                    return chain.proceed(request);
+                                }
+                            })
+                            .build();
+
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(BuildConfig.REMOTE_SERVICE_BASE_URL)
+                            .client(okHttpClient)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                            .build();
+
+                    ExtractionService service = retrofit.create(ExtractionService.class);
+
+                    instance = new ExtractionRemoteDataProvider(service);
+                }
+            }
+        }
+
+        return instance;
+    }
+
     @Override
-    public Observable<Result> getRecipeInformation(String url) {
+    public Observable<Result> getRecipeInformation(@NonNull String url) {
         return mExtractionService.extractRecipe(url)
                 .map(new Function<Response, Result>() {
                     @Override
