cc82e3b4b3fdc09ccf8f9d541df2309ea66ae14a
Tyler Madonna <developer@madonnaapps.com>
2020-02-04 20:38:25 -0500

    Added RecipeLocalDataSource data source interface

---
+++ app/src/main/java/com/inelasticcollision/recipelink/data/local/RecipeLocalDataSource.kt
@@ -0,0 +1,22 @@
+package com.inelasticcollision.recipelink.data.local
+
+import androidx.lifecycle.LiveData
+import com.inelasticcollision.recipelink.data.models.Recipe
+
+interface RecipeLocalDataSource {
+
+    fun getAllRecipes(): LiveData<List<Recipe>>
+
+    fun getFavoriteRecipes(): LiveData<List<Recipe>>
+
+    fun getRecipesBySearchTerm(searchTerm: String?): LiveData<List<Recipe>>
+
+    fun getRecipeById(id: String, callback: (Result<Recipe>) -> Unit)
+
+    fun saveRecipe(recipe: Recipe, callback: (Throwable?) -> Unit)
+
+    fun updateRecipe(recipe: Recipe, callback: (Throwable?) -> Unit)
+
+    fun deleteRecipe(id: String, callback: (Throwable?) -> Unit)
+
+}
