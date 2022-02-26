8cfa90b5b738722784853c15d335f1f7a650affb
Tyler Madonna <developer@madonnaapps.com>
2022-02-26 15:27:15 -0500

    Changed edit recipe to use DebounceTextContainer

--- app/src/main/java/com/inelasticcollision/recipelink/ui/fragment/editrecipe/EditRecipeFragment.kt
+++ app/src/main/java/com/inelasticcollision/recipelink/ui/fragment/editrecipe/EditRecipeFragment.kt
@@ -7,17 +7,21 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import coil.load
 import com.inelasticcollision.recipelink.R
 import com.inelasticcollision.recipelink.databinding.FragmentEditRecipeBinding
 import com.inelasticcollision.recipelink.ui.fragment.imagepicker.ImagePickerFragment
-import com.inelasticcollision.recipelink.ui.widget.DebouncedEditText
+import com.inelasticcollision.recipelink.ui.widget.DebounceTextContainer
 import com.inelasticcollision.recipelink.ui.widget.TextInputLayout
+import com.inelasticcollision.recipelink.util.textString
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
+class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe),
+    DebounceTextContainer.OnContentChangeListener,
+    TextInputLayout.OnTextInputChangeListener {
 
     // Properties
 
@@ -25,12 +29,17 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
 
     private val viewModel: EditRecipeViewModel by viewModels()
 
+    private var titleContainer: DebounceTextContainer? = null
+
+    private var notesContainer: DebounceTextContainer? = null
+
     // Lifecycle
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEditRecipeBinding.bind(view)
         setupToolbar()
+        setupViews()
         setupObservers()
     }
 
@@ -52,6 +61,33 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
 
         binding?.toolbar?.setNavigationOnClickListener {
             close()
+        }
+    }
+
+    private fun setupViews() {
+        binding?.titleEditText?.let {
+            titleContainer = DebounceTextContainer(it, lifecycleScope)
+            titleContainer?.onContentChangeListener = this
+        }
+
+        binding?.notesEditText?.let {
+            notesContainer = DebounceTextContainer(it, lifecycleScope)
+            notesContainer?.onContentChangeListener = this
+        }
+
+        binding?.tagsTextInputLayout?.coroutineScope = lifecycleScope
+        binding?.tagsTextInputLayout?.onTextInputChangedListener = this
+
+        binding?.saveFab?.setOnClickListener {
+            if (binding?.titleEditText?.textString != null) {
+                saveRecipe()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Recipes have to have a title", Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
         }
     }
 
@@ -98,66 +134,37 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
         }
 
         binding?.titleEditText?.textString = viewData.title
-        binding?.titleEditText?.editable = false
 
         binding?.notesEditText?.textString = viewData.notes
-        binding?.notesEditText?.editable = false
 
         binding?.tagsTextInputLayout?.setTextInput(viewData.tags ?: emptyList())
         binding?.tagsTextInputLayout?.editable = false
 
         if (viewData.favorite) {
-            binding?.toolbar?.menu?.findItem(R.id.menu_favorite)?.setIcon(R.drawable.ic_favorite)
+            binding?.toolbar?.menu?.findItem(R.id.menu_favorite)
+                ?.setIcon(R.drawable.ic_favorite)
         } else {
             binding?.toolbar?.menu?.findItem(R.id.menu_favorite)
                 ?.setIcon(R.drawable.ic_favorite_outline)
         }
 
         val imageUrls = viewData.imageUrls
-        setupViewListeners(imageUrls)
+        setupImageButtonListener(imageUrls)
 
-        binding?.changeImageButton?.isVisible = imageUrls != null && imageUrls.size > 1
+        binding?.changeImageButton?.isVisible = !imageUrls.isNullOrEmpty()
     }
 
     private fun renderErrorState() {
 
     }
 
-    private fun setupViewListeners(imageUrls: List<String>?) {
-        binding?.titleEditText?.onTextChangeListener =
-            object : DebouncedEditText.OnTextChangeListener {
-                override fun onTextChange(text: String?) {
-                    viewModel.setIntent(EditRecipeIntent.ChangeTitle(text))
-                }
-            }
-
-        binding?.notesEditText?.onTextChangeListener =
-            object : DebouncedEditText.OnTextChangeListener {
-                override fun onTextChange(text: String?) {
-                    viewModel.setIntent(EditRecipeIntent.ChangeNotes(text))
-                }
-            }
-
-        binding?.tagsTextInputLayout?.onTextInputChangedListener =
-            object : TextInputLayout.OnTextInputChangeListener {
-                override fun onTextInputChanged(text: List<String>) {
-                    viewModel.setIntent(EditRecipeIntent.ChangeTags(text))
-                }
-            }
-
-        binding?.saveFab?.setOnClickListener {
-            if (binding?.titleEditText?.textString != null) {
-                saveRecipe()
-            } else {
-                Toast.makeText(requireContext(), "Recipes have to have a title", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-
-        if (imageUrls != null && imageUrls.size > 1) {
+    private fun setupImageButtonListener(imageUrls: List<String>?) {
+        if (!imageUrls.isNullOrEmpty()) {
             binding?.changeImageButton?.setOnClickListener {
                 navigateToChooseImage(imageUrls)
             }
+        } else {
+            binding?.changeImageButton?.setOnClickListener(null)
         }
     }
 
@@ -177,5 +184,23 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
 
     private fun toggleFavorite() {
         viewModel.setIntent(EditRecipeIntent.ToggleFavorite)
+    }
+
+    // DebounceTextContainer.OnContentChangeListener
+
+    override fun onTextChange(container: DebounceTextContainer, text: String?) {
+        if (container.editText == binding?.titleEditText) {
+            viewModel.setIntent(EditRecipeIntent.ChangeTitle(text))
+        } else if (container.editText == binding?.notesEditText) {
+            viewModel.setIntent(EditRecipeIntent.ChangeNotes(text))
+        }
+    }
+
+    override fun onFocusChange(container: DebounceTextContainer, isFocused: Boolean) = Unit
+
+    // TextInputLayout.OnTextInputChangeListener
+
+    override fun onTextInputChanged(text: List<String>) {
+        viewModel.setIntent(EditRecipeIntent.ChangeTags(text))
     }
 }
