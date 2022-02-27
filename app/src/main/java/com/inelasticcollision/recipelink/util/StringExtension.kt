9c23a1fd97a0060d66cdc729ae204573566f2808
Tyler Madonna <developer@madonnaapps.com>
2022-02-27 10:34:01 -0500

    Replaced deprecated methods from string extension

--- app/src/main/java/com/inelasticcollision/recipelink/util/StringExtension.kt
+++ app/src/main/java/com/inelasticcollision/recipelink/util/StringExtension.kt
@@ -1,17 +1,13 @@
 package com.inelasticcollision.recipelink.util
 
-import java.util.*
-
 fun String.nullIfEmpty(): String? {
-    return if (this.isNotEmpty()) {
-        this
-    } else {
+    return this.ifEmpty {
         null
     }
 }
 
-fun String.capitalizeWords(locale: Locale = Locale.getDefault()): String {
-    return this.toLowerCase(locale)
+fun String.capitalizeWords(): String {
+    return this.lowercase()
         .split(" ")
-        .joinToString(" ") { word -> word.capitalize(locale) }
+        .joinToString(" ") { word -> word.replaceFirstChar { char -> char.uppercase() } }
 }
