0059d72f6527d55336d87a046125bf10bdda2c3a
Tyler Madonna <tyler@tylermadonna.com>
2022-10-13 20:31:23 -0400

    Added gesture navigation to add recipe

--- app/src/main/java/com/inelasticcollision/recipelink/ui/activity/addrecipe/AddRecipeActivity.kt
+++ app/src/main/java/com/inelasticcollision/recipelink/ui/activity/addrecipe/AddRecipeActivity.kt
@@ -1,8 +1,20 @@
 package com.inelasticcollision.recipelink.ui.activity.addrecipe
 
+import android.os.Build
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.inelasticcollision.recipelink.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AddRecipeActivity : AppCompatActivity(R.layout.activity_add_recipe)
+class AddRecipeActivity : AppCompatActivity(R.layout.activity_add_recipe) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+}
